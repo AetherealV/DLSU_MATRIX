76a3f7252d862cff87700786785dc166119ee2b4
AetherealV <140776018+AetherealV@users.noreply.github.com>
2026-09-19 15:43:05 +0800

    Update tt_um_vga_glyph_mode.v

--- src/tt_um_vga_glyph_mode.v
+++ src/tt_um_vga_glyph_mode.v
@@ -50,7 +50,7 @@ module tt_um_vga_glyph_mode(
     hvsync_generator hvsync_gen(
         .clk(clk),
         .reset(~rst_n),
-        .mode(2'b11),
+        .mode(ui_in[7:6]),
         .hsync(hsync),
         .vsync(vsync),
         .display_on(display_on),
